1dcc093bd783ef441fd5438eaac4314711ed28bb
Asaduzzaman Alamin <asaduzzamanrossow2000@gmail.com>
2022-06-25 03:52:15 +0600

    ok-to-fine

--- src/componants/Landing.js
+++ src/componants/Landing.js
@@ -20,7 +20,7 @@ const Landing = () => {
             <h1 data-aos="fade-up"
      data-aos-duration="1000" class="mb-5 text-5xl font-bold font-mono"><span className='text-pink-600'>Hello</span> Dude!! <br /> <span className='text-yellow-600'>Welcome</span> fancy <span className='text-pink-600'>Mobile</span>  Mania</h1>
             <p data-aos="fade-up"
-     data-aos-duration="1000"  data-aos-delay="1000" class="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut <br /> assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+     data-aos-duration="1000"  data-aos-delay="1000" class="mb-5 text-justify font-mono">This is our mobile werehouse Website if you need any phone you just <br />  follow and eplement our website i beleive that you have choose a great mobailefollow and eplement our website i beleive that you have choose a great mobailefollow and eplement our website i beleive that you have choose a great mobailefollow and eplement our website i beleive that you have choose a great mobaile</p>
             <button data-aos="flip-left"
      data-aos-easing="ease-out-cubic"
      data-aos-duration="2000" class="btn btn-primary font-bold font-mono text-accent">Get Started <FaHourglassStart></FaHourglassStart></button>
